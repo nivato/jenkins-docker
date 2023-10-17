c7d1277f15c31cfeb52620e16ea6e92933410a5a
Nazar Ivato <nazarivato@gmail.com>
2023-10-17 21:11:59 +0300

    Fixed index.php page

--- src/index.php
+++ src/index.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Apache 2 on the Ubuntu 20.04</title>
+    <title>Apache 2 on the Alpine Linux 3.17</title>
 </head>
-<body>
+<body style="color: #555555; background-color: #eeeeee; padding: 0;">
     <div style="width: 900px; margin: 0 auto 0 auto;">
-        <div style="text-align: center; background-color: #304558; padding: 5px; margin-bottom: 30px;">
+        <div style="text-align: center; background-color: #304558; padding: 5px; margin-bottom: 5px;">
             <img width="800" src="https://httpd.apache.org/images/httpd_logo_wide_new.png" />
+        </div>
+        <div style="text-align: center; background-color: white; padding: 5px; margin-bottom: 30px;">
+            <img width="300" src="https://www.alpinelinux.org/alpinelinux-logo.svg" />
         </div>
 
         <h2 style="color: #304558;">🌎 Hello World!</h2>
-        <h3>This is <code style="color: #304558;">Apache 2</code> on the <code style="color: #e95420;">Ubuntu 20.04</code> Docker image.</h2>
+        <h3>This is <code style="color: #304558;">Apache 2</code> on the <code style="color: #e95420;">Alpine Linux 3.17</code> Docker image.</h2>
         <?php
             $server_ip = $_SERVER['SERVER_ADDR'];
             echo "<h3>Server IP Address is: <code> $server_ip </code></h3>"
